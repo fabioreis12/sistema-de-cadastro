9f94b698123ee159a6e2d712bac4459503fda7f5
Fabio Reis <118979260+fabioreis12@users.noreply.github.com>
2023-10-05 22:18:20 -0300

    Adicionando comando de banco de dados no arquivo app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Iniciando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+// mais um console log de teste
+console.log("Conexão com o banco de dados realizada com sucesso")
