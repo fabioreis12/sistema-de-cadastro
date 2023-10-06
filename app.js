d5b2da865b6749cf2b0f1c1b9fd942bfe63cc1fd
Fabio Souza <fabiodesouza12@gmail.com>
2023-10-05 22:23:58 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Iniciando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console log de teste
 console.log("Conexão com o banco de dados realizada com sucesso")
+console.log("Enviando para o GitHub")
